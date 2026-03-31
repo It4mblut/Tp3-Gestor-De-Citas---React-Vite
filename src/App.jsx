41c4c0444b5a0587e6b50464b61a6b5a38fafcf3
Android Studio <devandroid@ort>
2026-03-31 14:55:33 -0300

    Avanzando con los css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+ <link rel="stylesheet" href="App.css"></link>
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      //Acá se supone que solo llame a las otras cosas q voy a crear
+    <title>Administración de Pacientes</title>
+    
+      Acá se supone que solo llame a las otras cosas q voy a crear
 
     </>
   )
