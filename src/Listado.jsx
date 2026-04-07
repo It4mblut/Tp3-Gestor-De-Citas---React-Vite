c30e92b024fd520dc52ce23e1e01f5a35a1a8e18
EITAN URIEL KOZE <49433681@ort>
2026-04-07 15:41:41 -0300

    Ahora si, las citas tienen fondo blanco

--- src/Listado.jsx
+++ src/Listado.jsx
@@ -1,6 +1,5 @@
 import './Listado.css'
-import { useState } from "react";
-
+import './Cita.css'
 
 function Listado({ citas }) {
   return (
@@ -10,7 +9,7 @@ function Listado({ citas }) {
       {citas.map((cita, index) => (
         <div className="cita" key={index}>
           <p>Mascota: <span>{cita.mascota}</span></p>
-          <p>Dueño: <span>{cita.dueño}</span></p>
+          <p>Dueño: <span>{cita.propietario}</span></p>
           <p>Fecha: <span>{cita.fecha}</span></p>
           <p>Hora: <span>{cita.hora}</span></p>
           <p>Sintomas: <span>{cita.sintomas}</span></p>
